8465744118828dcbe63a7f6ad0c9379e067f812c
Eremeow138 <eremeow138@yandex.ru>
2022-04-12 11:08:36 +0800

    feat: add counter updating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ngrx-for-beginners';
+  public counter = 0;
+
+  updatedAt?: number;
+
+  get cannotDecrease(): boolean {
+    return this.counter <= 0;
+  }
+
+  public increase(): void {
+    this.updatedAt = Date.now();
+    this.counter++;
+  }
+  public decrease(): void {
+    this.updatedAt = Date.now();
+    this.counter--;
+  }
+  public clear(): void {
+    this.updatedAt = Date.now();
+    this.counter = 0;
+  }
 }
